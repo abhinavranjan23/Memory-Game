cd081f0e5da5deb96ec6e55b7c8df1145e05860e
abhinavranjan23 <abhinavranjan2202@gmail.com>
2025-09-07 23:37:16 +0530

    favicon icon

--- memory-card-game/client/api/sitemap.xml.js
+++ memory-card-game/client/api/sitemap.xml.js
@@ -1,7 +1,8 @@
 export default function handler(req, res) {
-  // Set proper headers for XML
+  // Set proper headers for XML sitemap
   res.setHeader("Content-Type", "application/xml; charset=utf-8");
   res.setHeader("Cache-Control", "public, max-age=3600");
+  res.setHeader("X-Content-Type-Options", "nosniff");
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
